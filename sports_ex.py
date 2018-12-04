b9cab128ffeb02b40905078434cefbb7d31744ef
Muneeb Syed <msyed2@students.kennesaw.edu>
2018-12-04 12:06:59 -0500

    typed in sports 12-15
    sports for 12-15

--- sports_ex.py
+++ sports_ex.py
@@ -4,19 +4,19 @@ import sports
 def main():
     print('Welcome to the Sports Statistics Program!\n')
     print('I can provide you with all types of scores!\n')
-    print('What sport or team would you like scores for? (type number)\n')
+    print('What sport would you like scores for? (type number)\n')
     print('1. Football\n2. Soccer\n3. Basketball\n4. Baseball\n5. Hockey\n6. Tennis\n7. Rugby-League\n8. Volleyball\n9. Cricket\n10. Handball\n11. Rugby-Union\n 12. Type in Team \n')
-    sport = input('Enter a number: ')
+    sport = input('Q for quit or a number: ')
     
     while True:
-        if sport.isdigit(type in any name of basketball team):
-            sport = int(type in any name of football team)
-        if sport == "Q": type in any name of baseball team 
-            print('Thanks for using the Sports Statistics Program.') type in any name of soccer team
+        if sport.isdigit():
+            sport = int(sport)
+        if sport == "Q":
+            print('Thanks for using the Sports Statistics Program.')
             break
         else:
             get_scores(sport)
-            sport = str(input('Enter another number or enter Q to quit: ')) 
+            sport = str(input('Q for quit or another number: ')) 
 
 def get_scores(sport):
     if sport == 1:
@@ -43,16 +43,16 @@ def get_scores(sport):
     elif sport == 11:
         scores = sports.get_sport(sports.RUGBY_U)
     elif sport == 12:  # this is for specific teams
-        team_name = str(input("Type the name of the baseball team: "))
+        team_name = str(input("Type the name of the baseball team"))
         team_data = sports.get_team("baseball", team_name)
     elif sport == 13:  # this is for specific teams
-        team_name = str(input("Type the name of the football team: "))
+        team_name = str(input("Type the name of the football team"))
         team_data = sports.get_team("football", team_name)
     elif sport == 14:  # this is for specific teams
-        team_name = str(input("Type the name of the hockey team: "))
+        team_name = str(input("Type the name of the hockey team"))
         team_data = sports.get_team("hockey", team_name)
     elif sport == 15:  # this is for specific teams
-        team_name = str(input("Type the name of the basketball team: "))
+        team_name = str(input("Type the name of the basketball team"))
         team_data = sports.get_team("basketball", team_name)
 
     if sport < 12:  # if it is just a sport     
